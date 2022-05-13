d3917fbf111034ad2bfcaaf4213c7fa9432bb8af
Felipe Lincoln <felipelincoln@usp.br>
2022-05-13 02:10:09 -0300

    create tests

--- test/is-monkey-naked-tests.js
+++ test/is-monkey-naked-tests.js
@@ -1,7 +1,22 @@
-const { fetch } = require("node-fetch")
+const fs = require("fs")
 
 const { expect } = require("chai")
 const { ethers, waffle } = require("hardhat")
+
+const nakedIds =
+  fs.readFileSync("./test/ocm-metadata.csv")
+    .toString()
+    .split("\n")
+    .map(row => row.split(","))
+    .filter(([, hat, , clothes, , earrings, ..._]) => hat + clothes + earrings == "000")
+    .map(([id, ..._]) => id)
+
+const notNakedIds =
+  [...Array(10001).keys()]
+    .map(x => x.toString())
+    .filter(x => !nakedIds.includes(x))
+
+console.log(`There are ${nakedIds.length} naked monkeys.`)
 
 before(async function() {
   // deploy OCM contract
@@ -15,11 +30,26 @@ before(async function() {
   // deploy IsMonkeyNaked contract
   let IsMonkeyNaked = await ethers.getContractFactory("IsMonkeyNaked", {libraries: {BytesUtils: bytesUtilsAddress}})
   this.isMonkeyNaked = await IsMonkeyNaked.deploy(onChainMonkeyAddress)
-
-  this.a = await fetch("https://onchainmonkey.com/ocm-metadata.csv")
-
 })
 
-it("test", function(){
-  console.log(this.a)
+describe("isNaked", function(){
+  it("successfully returns true for all naked monkeys", async function(){
+    for(const id of nakedIds){
+      expect(await this.isMonkeyNaked.isNaked(id)).to.true
+      break
+    }
+  })
+
+  it("successfully returns false for not naked monkeys", async function(){
+    for(const id of nakedIds){
+      expect(await this.isMonkeyNaked.isNaked(id)).to.true
+      break
+    }
+  })
+
+  it("reverts when tokenId is less than 0 or greater than 12839", async function(){
+    await expect(this.isMonkeyNaked.isNaked(-1)).to.reverted
+    await expect(this.isMonkeyNaked.isNaked(12840)).to.reverted
+  })
+
 })
